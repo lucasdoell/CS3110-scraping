7cb2cdde75633bb6c82c2a285b07bd0df75a52a8
Dustin Phong Ngo <dpn29@cornell.edu>
2022-10-21 16:33:24 -0400

    Update sport.mli

--- src/sport.mli
+++ src/sport.mli
@@ -6,28 +6,28 @@ exception UnknownStat of string
 (** Raised when an unknown stat identifier is encountered. It carries
     the identifier of the unknown stat*)
 
-type Basketball_Player
+type basketball_player
 (** The record type representing a basketball player*)
 
-type Baseball_Player
+type baseball_player
 (** The record type representing a baseball player*)
 
-type Baseball_Pitcher
+type baseball_pitcher
 (** The record type representing a baseball pitcher*)
 
 val is_sport : string -> bool
 (** [is_sport r] verifies that r is a compatible sport, raises
     [UnknownSport r] if r is not a sport supported by the system *)
 
-val basketball_stat : Basketball_Player -> string -> float
+val basketball_stat : basketball_player -> string -> float
 (** [basketball_stat p s] gives the requested stat [stat]
     of player [player], raises [UnknownStat s] if player [p] doesn't
     have requested stat*)
 
-val baseball_stat : Baseball_Player -> string -> float
+val baseball_stat : baseball_player -> string -> float
 (** [baseball_stat p s] gives the requested stat [s] of player [p], raises
     [UnknownStat s] if player [p] doesn't have requested stat*)
 
-val pitcher_stat : Baseball_Pitcher -> string -> float
+val pitcher_stat : baseball_pitcher -> string -> float
 (** [pitcher_stat p s] gives the requested stat [s] of player [p], raises
     [UnknownStat s] if player [p] doesn't have requested stat*)
