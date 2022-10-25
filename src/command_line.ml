a17d28091c08f00927bb2d49276f9af7216ebc57
Jeffrey Namnum <jn383@cornell.edu>
2022-10-24 20:13:13 -0400

    minor text changes

--- src/command_line.ml
+++ src/command_line.ml
@@ -2,8 +2,8 @@ open ANSITerminal
 
 let main () =
   ANSITerminal.print_string [ ANSITerminal.red ]
-    "\n\nWelcome to the 3110 Text Adventure Game engine.\n";
-  print_endline "Please enter the name of the game file you want to load.\n";
+    "\n\nPlaceholder\n";
+  print_endline "Please enter the sport.\n";
   match read_line () with
   | exception End_of_file -> ()
   | file_name -> print_endline "e"
