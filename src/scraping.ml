5e410e89f6a9016a5a35ec219d9fea2bf088747a
Lucas Doell <lmd248@cornell.edu>
2022-11-30 00:24:18 -0500

    feat: add support for filtering football types
    - refactor basketball string results
    - add support for football
    - add queries for quarterback and runningback
    - add types to support queries for different types of players

--- src/scraping.ml
+++ src/scraping.ml
@@ -4,60 +4,405 @@ open Batteries
 module M = Agent.Monad
 open M.Infix
 
-type bball_res = {
-  adv : string;
-  misc : string;
-  def : string;
-  assists : string;
-  shooting : string;
-  reb : string;
-  scoring : string;
-}
+module type BBall = sig
+  type bball_res
 
-let init_bball_scrape (player : string) =
-  let soup = read_file "./data/query.html" |> parse in
-  (* Teporary static file to test basic functionality *)
-  let query = soup $ "div" in
-  let result = trimmed_texts query in
-  String.concat "'; '" (List.rev result)
+  val init_bball_scrape : string -> string
+  val filter_bball_scrape : string -> bball_res
+  val bball_scrape : string -> bball_res
+  val to_string : bball_res -> string
+end
 
-let filter_bball_scrape res =
-  let adv =
-    Substring.to_string
-      (Substring.substring res (String.find res "'USG%'; '") 14)
-  in
-  let misc =
-    Substring.to_string
-      (Substring.substring res (String.find res "'+/-'; '") 13)
-  in
-  let def =
-    Substring.to_string
-      (Substring.substring res (String.find res "'BPG'; '") 13)
-  in
-  let assists =
-    Substring.to_string
-      (Substring.substring res (String.find res "'APG'; '") 13)
-  in
-  let shooting =
-    Substring.to_string
-      (Substring.substring res (String.find res "'EFG%'; '") 15)
-  in
-  let reb =
-    Substring.to_string
-      (Substring.substring res (String.find res "'RPG'; '") 14)
-  in
-  let scoring =
-    Substring.to_string
-      (Substring.substring res (String.find res "'PPG'; '") 14)
-  in
+module type FBall = sig
+  type quarterback
+  type offensive
+  type support
+  type hybrid
+  type safety
+  type tackler
+  type kicker
+  type punter
+  type player
 
-  { adv; misc; def; assists; shooting; reb; scoring }
+  val init_fball_scrape : string -> string
+  val get_player_info : string -> string -> player
+  val filter_qback_scrape : string -> quarterback
+  val filter_off_scrape : string -> offensive
+  val qback_scrape : string -> quarterback
+  val off_scrape : string -> offensive
+  val to_string_qb : quarterback -> string
+  val to_string_off : offensive -> string
+end
 
-let bball_scrape (player : string) =
-  filter_bball_scrape (init_bball_scrape player)
+module Basketball : BBall = struct
+  type bball_res = {
+    adv : string;
+    misc : string;
+    def : string;
+    assists : string;
+    shooting : string;
+    reb : string;
+    scoring : string;
+  }
 
-let to_string res =
-  "Scoring: " ^ res.scoring ^ "\n" ^ "Rebounding: " ^ res.reb ^ "\n"
-  ^ "Shooting: " ^ res.shooting ^ "\n" ^ "Assists: " ^ res.assists ^ "\n"
-  ^ "Defense: " ^ res.def ^ "\n" ^ "Misc: " ^ res.misc ^ "\n" ^ "Advanced: "
-  ^ res.adv
+  let init_bball_scrape (player : string) =
+    let soup = read_file "./data/query.html" |> parse in
+    (* Teporary static file to test basic functionality *)
+    let query = soup $ "div" in
+    let result = trimmed_texts query in
+    String.concat "'; '" (List.rev result)
+
+  let filter_bball_scrape res =
+    let adv =
+      Substring.to_string
+        (Substring.substring res
+           (String.find res "'USG%'; '" + 9)
+           (String.index_from res (String.find res "'USG%'; '" + 9) '\''
+           - (String.find res "'USG%'; '" + 9)))
+    in
+    let misc =
+      Substring.to_string
+        (Substring.substring res
+           (String.find res "'+/-'; '" + 8)
+           (String.index_from res (String.find res "'+/-'; '" + 8) '\''
+           - (String.find res "'+/-'; '" + 8)))
+    in
+    let def =
+      Substring.to_string
+        (Substring.substring res
+           (String.find res "'BPG'; '" + 8)
+           (String.index_from res (String.find res "'BPG'; '" + 8) '\''
+           - (String.find res "'BPG'; '" + 8)))
+    in
+    let assists =
+      Substring.to_string
+        (Substring.substring res
+           (String.find res "'APG'; '" + 8)
+           (String.index_from res (String.find res "'APG'; '" + 8) '\''
+           - (String.find res "'APG'; '" + 8)))
+    in
+    let shooting =
+      Substring.to_string
+        (Substring.substring res
+           (String.find res "'EFG%'; '" + 9)
+           (String.index_from res (String.find res "'EFG%'; '" + 9) '\''
+           - (String.find res "'EFG%'; '" + 9)))
+    in
+    let reb =
+      Substring.to_string
+        (Substring.substring res
+           (String.find res "'RPG'; '" + 8)
+           (String.index_from res (String.find res "'RPG'; '" + 8) '\''
+           - (String.find res "'RPG'; '" + 8)))
+    in
+    let scoring =
+      Substring.to_string
+        (Substring.substring res
+           (String.find res "'PPG'; '" + 8)
+           (String.index_from res (String.find res "'PPG'; '" + 8) '\''
+           - (String.find res "'PPG'; '" + 8)))
+    in
+
+    { adv; misc; def; assists; shooting; reb; scoring }
+
+  let bball_scrape (player : string) =
+    filter_bball_scrape (init_bball_scrape player)
+
+  let to_string res =
+    "Scoring: " ^ res.scoring ^ "\n" ^ "Rebounding: " ^ res.reb ^ "\n"
+    ^ "Shooting: " ^ res.shooting ^ "\n" ^ "Assists: " ^ res.assists ^ "\n"
+    ^ "Defense: " ^ res.def ^ "\n" ^ "Misc: " ^ res.misc ^ "\n" ^ "Advanced: "
+    ^ res.adv
+end
+
+module Football = struct
+  type quarterback = {
+    yds : string;
+    td : string;
+    ints : string;
+    pyds : string;
+    ptd : string;
+    ryds : string;
+    rtd : string;
+  }
+
+  type offensive = {
+    yds : string;
+    att : string;
+    td : string;
+    ryds : string;
+    rtd : string;
+    recyds : string;
+    rectd : string;
+    kretyds : string;
+    pretyds : string;
+    pts : string;
+    apyds : string;
+  }
+
+  type support = {
+    gs : string;
+    gp : string;
+  }
+
+  type hybrid = {
+    yds : string;
+    recs : string;
+    td : string;
+    recyds : string;
+    rectd : string;
+    ryds : string;
+    rtd : string;
+    kretyds : string;
+    pretyds : string;
+    pts : string;
+    apyds : string;
+  }
+
+  type safety = {
+    tckl : string;
+    ints : string;
+    sck : string;
+    kretyds : string;
+    pretyds : string;
+    pts : string;
+    apyds : string;
+  }
+
+  type tackler = {
+    tckl : string;
+    ints : string;
+    sck : string;
+  }
+
+  type kicker = {
+    fga : string;
+    fgm : string;
+    pat : string;
+    pts : string;
+    tb : string;
+    netavg : string;
+  }
+
+  type punter = {
+    avg : string;
+    punts : string;
+    in20 : string;
+    pts : string;
+    tb : string;
+    netavg : string;
+  }
+
+  (* type position = | QB of quarterback | RB of offensive | G of support | T of
+     support | OL of support | C of support | TE of hybrid | FB of offensive |
+     WR of hybrid | S of safety | DB of safety | LB of tackler | DT of tackler |
+     NT of tackler | CB of safety | K of kicker | P of punter *)
+
+  type player = {
+    name : string;
+    position : string;
+    number : string;
+    team : string;
+  }
+
+  let init_fball_scrape (player : string) =
+    let soup = read_file ("./" ^ player) |> parse in
+    (* Teporary static file to test basic functionality *)
+    let query = soup $ "div" in
+    let result = trimmed_texts query in
+    String.concat "'; '" (List.rev result)
+
+  let get_player_info player res =
+    let player = player |> String.uppercase_ascii in
+    let name =
+      Substring.to_string
+        (Substring.substring res (String.find res player) (String.length player))
+    in
+    let info =
+      Substring.to_string
+        (Substring.substring res (String.find res "#")
+           (String.index_from res (String.find res "#") '\''
+           - String.find res "#"))
+    in
+    let number = Substring.to_string (Substring.substring info 1 2) in
+    let position =
+      Substring.to_string
+        (Substring.substring info
+           (String.find info "-" + 2)
+           (String.index_from info (String.find info "-" + 1) '-'
+           - String.find info "-" - 3))
+    in
+    let team =
+      Substring.to_string
+        (Substring.substring
+           (Substring.to_string
+              (Substring.substring info
+                 (String.index_from info (String.find info "-" + 1) '-'
+                 - String.find info "-" + 2)
+                 (String.length info
+                 - (String.index_from info (String.find info "-" + 1) '-'
+                   - String.find info "-" + 2))))
+           3
+           (String.length
+              (Substring.to_string
+                 (Substring.substring info
+                    (String.index_from info (String.find info "-" + 1) '-'
+                    - String.find info "-" + 2)
+                    (String.length info
+                    - (String.index_from info (String.find info "-" + 1) '-'
+                      - String.find info "-" + 3))))
+           - 2))
+    in
+    { name; position; number; team }
+
+  let filter_qback_scrape res =
+    let yds =
+      Substring.to_string
+        (Substring.substring res
+           (String.find res "'YDS'; '" + 8)
+           (String.index_from res (String.find res "'YDS'; '" + 8) '\''
+           - (String.find res "'YDS'; '" + 8)))
+    in
+    let td =
+      Substring.to_string
+        (Substring.substring res
+           (String.find res "'TD'; '" + 7)
+           (String.index_from res (String.find res "'TD'; '" + 7) '\''
+           - (String.find res "'TD'; '" + 7)))
+    in
+    let ints =
+      Substring.to_string
+        (Substring.substring res
+           (String.find res "'INT'; '" + 8)
+           (String.index_from res (String.find res "'INT'; '" + 8) '\''
+           - (String.find res "'INT'; '" + 8)))
+    in
+    let pyds =
+      Substring.to_string
+        (Substring.substring res
+           (String.find res "'PYDS'; '" + 9)
+           (String.index_from res (String.find res "'PYDS'; '" + 9) '\''
+           - (String.find res "'PYDS'; '" + 9)))
+    in
+    let ptd =
+      Substring.to_string
+        (Substring.substring res
+           (String.find res "'PTD'; '" + 8)
+           (String.index_from res (String.find res "'PTD'; '" + 8) '\''
+           - (String.find res "'PTD'; '" + 8)))
+    in
+    let ryds =
+      Substring.to_string
+        (Substring.substring res
+           (String.find res "'RYDS'; '" + 9)
+           (String.index_from res (String.find res "'RYDS'; '" + 9) '\''
+           - (String.find res "'RYDS'; '" + 9)))
+    in
+    let rtd =
+      Substring.to_string
+        (Substring.substring res
+           (String.find res "'RTD'; '" + 8)
+           (String.index_from res (String.find res "'RTD'; '" + 8) '\''
+           - (String.find res "'RTD'; '" + 8)))
+    in
+
+    { yds; td; ints; pyds; ptd; ryds; rtd }
+
+  let filter_off_scrape res =
+    let yds =
+      Substring.to_string
+        (Substring.substring res
+           (String.find res "'YDS'; '" + 8)
+           (String.index_from res (String.find res "'YDS'; '" + 8) '\''
+           - (String.find res "'YDS'; '" + 8)))
+    in
+    let att =
+      Substring.to_string
+        (Substring.substring res
+           (String.find res "'ATT'; '" + 8)
+           (String.index_from res (String.find res "'ATT'; '" + 8) '\''
+           - (String.find res "'ATT'; '" + 8)))
+    in
+    let td =
+      Substring.to_string
+        (Substring.substring res
+           (String.find res "'TD'; '" + 7)
+           (String.index_from res (String.find res "'TD'; '" + 7) '\''
+           - (String.find res "'TD'; '" + 7)))
+    in
+    let ryds =
+      Substring.to_string
+        (Substring.substring res
+           (String.find res "'RYDS'; '" + 9)
+           (String.index_from res (String.find res "'RYDS'; '" + 9) '\''
+           - (String.find res "'RYDS'; '" + 9)))
+    in
+    let rtd =
+      Substring.to_string
+        (Substring.substring res
+           (String.find res "'RTD'; '" + 8)
+           (String.index_from res (String.find res "'RTD'; '" + 8) '\''
+           - (String.find res "'RTD'; '" + 8)))
+    in
+    let recyds =
+      Substring.to_string
+        (Substring.substring res
+           (String.find res "'RECYDS'; '" + 11)
+           (String.index_from res (String.find res "'RECYDS'; '" + 11) '\''
+           - (String.find res "'RECYDS'; '" + 11)))
+    in
+    let rectd =
+      Substring.to_string
+        (Substring.substring res
+           (String.find res "'RECTD'; '" + 10)
+           (String.index_from res (String.find res "'RECTD'; '" + 10) '\''
+           - (String.find res "'RECTD'; '" + 10)))
+    in
+    let kretyds =
+      Substring.to_string
+        (Substring.substring res
+           (String.find res "'K-RET YDS'; '" + 14)
+           (String.index_from res (String.find res "'K-RET YDS'; '" + 14) '\''
+           - (String.find res "'K-RET YDS'; '" + 14)))
+    in
+    let pretyds =
+      Substring.to_string
+        (Substring.substring res
+           (String.find res "'P-RET YDS'; '" + 14)
+           (String.index_from res (String.find res "'P-RET YDS'; '" + 14) '\''
+           - (String.find res "'P-RET YDS'; '" + 14)))
+    in
+    let pts =
+      Substring.to_string
+        (Substring.substring res
+           (String.find res "'PTS'; '" + 8)
+           (String.index_from res (String.find res "'PTS'; '" + 8) '\''
+           - (String.find res "'PTS'; '" + 8)))
+    in
+    let apyds =
+      Substring.to_string
+        (Substring.substring res
+           (String.find res "'AP YDS'; '" + 11)
+           (String.index_from res (String.find res "'AP YDS'; '" + 11) '\''
+           - (String.find res "'AP YDS'; '" + 11)))
+    in
+    { yds; att; td; ryds; rtd; recyds; rectd; kretyds; pretyds; pts; apyds }
+
+  let qback_scrape (player : string) =
+    filter_qback_scrape (init_fball_scrape player)
+
+  let off_scrape (player : string) =
+    filter_off_scrape (init_fball_scrape player)
+
+  let to_string_qb (res : quarterback) =
+    "YDS: " ^ res.yds ^ "\n" ^ "TD: " ^ res.td ^ "\n" ^ "INT: " ^ res.ints
+    ^ "\n" ^ "PYDS: " ^ res.pyds ^ "\n" ^ "PTD: " ^ res.ptd ^ "\n" ^ "RYDS: "
+    ^ res.ryds ^ "\n" ^ "RTD: " ^ res.rtd
+
+  let to_string_off (res : offensive) =
+    "YDS: " ^ res.yds ^ "\n" ^ "ATT: " ^ res.att ^ "\n" ^ "TD: " ^ res.td ^ "\n"
+    ^ "RYDS: " ^ res.ryds ^ "\n" ^ "RTD: " ^ res.rtd ^ "\n" ^ "RECYDS: "
+    ^ res.recyds ^ "\n" ^ "RECTD: " ^ res.rectd ^ "\n" ^ "K-RET YDS: "
+    ^ res.kretyds ^ "\n" ^ "P-RET YDS: " ^ res.pretyds ^ "\n" ^ "PTS: "
+    ^ res.pts ^ "\n" ^ "AP YDS: " ^ res.apyds
+end
--- test/main.ml
+++ test/main.ml
@@ -1,11 +1,23 @@
 open Mechaml
 open OUnit2
 open Scraping
-open CommandLine
-open ANSITerminal
+(* open CommandLine open ANSITerminal *)
 
 let test_command_line = ()
 let _ = print_endline "Testing HTML parsing:"
-let _ = print_endline "Testing [data/query.html]"
-let _ = print_endline "\nPlayer: Lebron James"
-let test_bball_scrape = bball_scrape "lebron" |> to_string |> print_endline
+
+let test_bball_scrape =
+  let _ = print_endline "\n➾ Testing [data/query.html]" in
+  let _ = print_endline "\nPlayer: Lebron James" in
+  Basketball.bball_scrape "data/query.html"
+  |> Basketball.to_string |> print_endline
+
+let test_qb_scrape =
+  let _ = print_endline "\n➾ Testing [data/qb.html]" in
+  let _ = print_endline "\nPlayer: Tom Brady" in
+  Football.qback_scrape "data/qb.html" |> Football.to_string_qb |> print_endline
+
+let test_rb_scrape =
+  let _ = print_endline "\n➾ Testing [data/rb.html]" in
+  let _ = print_endline "\nPlayer: Leonard Fournette" in
+  Football.off_scrape "data/rb.html" |> Football.to_string_off |> print_endline
