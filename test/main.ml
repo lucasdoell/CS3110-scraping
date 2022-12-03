8d371e0e1c83542eb27bc1db98d5ca4307a3b4a2
Lucas Doell <lmd248@cornell.edu>
2022-12-03 01:24:14 -0500

    refactor: test suite change for less duplication

--- test/main.ml
+++ test/main.ml
@@ -7,30 +7,23 @@ let test_command_line = ()
 let _ = print_endline "Testing Scraping:"
 
 let scraping_test_suite =
+  let name = "lebron james" in
   let _ = print_endline "\n➾ Testing Basketball:\n" in
-  let _ =
-    print_endline (to_string_player (Basketball.get_player_info "Lebron James"))
-  in
-  Basketball.bball_scrape "lebron james"
-  |> Basketball.to_string |> print_endline;
+  let _ = print_endline (to_string_player (Basketball.get_player_info name)) in
+  Basketball.bball_scrape name |> Basketball.to_string |> print_endline;
 
+  let name = "kevin durant" in
   let _ =
-    print_endline
-      ("\n" ^ to_string_player (Basketball.get_player_info "Kevin Durant"))
+    print_endline ("\n" ^ to_string_player (Basketball.get_player_info name))
   in
-  Basketball.bball_scrape "kevin durant"
-  |> Basketball.to_string |> print_endline;
+  Basketball.bball_scrape name |> Basketball.to_string |> print_endline;
 
   let _ = print_endline "\n➾ Testing Quarterback\n" in
-  let _ =
-    print_endline (to_string_player (Football.get_player_info "Tom Brady"))
-  in
+  let name = "tom brady" in
+  let _ = print_endline (to_string_player (Football.get_player_info name)) in
   Football.qback_scrape "tom brady" |> Football.to_string_qb |> print_endline;
 
   let _ = print_endline "\n➾ Testing Runningback\n" in
-  let _ =
-    print_endline
-      (to_string_player (Football.get_player_info "Leonard Fournette"))
-  in
-  Football.off_scrape "Leonard Fournette"
-  |> Football.to_string_off |> print_endline
+  let name = "leonard fournette" in
+  let _ = print_endline (to_string_player (Football.get_player_info name)) in
+  Football.off_scrape name |> Football.to_string_off |> print_endline
