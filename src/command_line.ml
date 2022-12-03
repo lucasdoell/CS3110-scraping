08ca7109536fd0feea3c3e0df9c8b8e346112944
Lucas Doell <lmd248@cornell.edu>
2022-12-03 01:40:48 -0500

    revert formatting changes

--- src/command_line.ml
+++ src/command_line.ml
@@ -1,43 +1,19 @@
 open ANSITerminal
 
 let main () =
-
   ANSITerminal.print_string [ ANSITerminal.red ]
-
-    "\n\nWelcome to
-\n   the 3110 Sports Database.\n";
-
-  print_endline
-
-    "Please enter the name of the
-\n\
-
-    \   league you would like to see.\n\
-
-    \ Supported leagues: NBA\n";
-
+    "\n\nWelcome to the 3110 Sports Database.\n";
+  print_endline "Please enter the name of the league you would like to see.\n
+                 Supported leagues: NBA\n";
   match read_line () with
-
   | exception No_Input -> ()
-
   | "NBA" -> nba
 
-
 let nba =
-
-  print_endline
-
-    "What player would you like to know about? Please
-\n\
-
-    \   give their full first and last name\n";
-
+  print_endline "What player would you like to know about? Please give their
+                 full first and last name\n";
   match read_line () with
-
   | exception No_Input -> nba
-
   | player -> bball_scrape player
 
-
 let () = main ()
-
