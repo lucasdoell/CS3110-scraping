2ca0ea7148790a0a80cbac98ebdf62ee6d5b477d
Lucas Doell <lmd248@cornell.edu>
2022-12-03 04:48:14 -0500

    fix: rename scraping.ml to scrape.ml
    - make doc did not work properly
    - this change fixes the auto generated docs

--- test/main.ml
+++ test/main.ml
@@ -9,21 +9,35 @@ let _ = print_endline "Testing Scraping:"
 let scraping_test_suite =
   let name = "lebron james" in
   let _ = print_endline "\n➾ Testing Basketball:\n" in
-  let _ = print_endline (to_string_player (Basketball.get_player_info name)) in
-  Basketball.bball_scrape name |> Basketball.to_string |> print_endline;
+  let _ =
+    print_endline
+      (Scrape.to_string_player (Scrape.Basketball.get_player_info name))
+  in
+  Scrape.Basketball.bball_scrape name
+  |> Scrape.Basketball.to_string |> print_endline;
 
   let name = "kevin durant" in
   let _ =
-    print_endline ("\n" ^ to_string_player (Basketball.get_player_info name))
+    print_endline
+      ("\n" ^ Scrape.to_string_player (Scrape.Basketball.get_player_info name))
   in
-  Basketball.bball_scrape name |> Basketball.to_string |> print_endline;
+  Scrape.Basketball.bball_scrape name
+  |> Scrape.Basketball.to_string |> print_endline;
 
   let _ = print_endline "\n➾ Testing Quarterback\n" in
   let name = "tom brady" in
-  let _ = print_endline (to_string_player (Football.get_player_info name)) in
-  Football.qback_scrape name |> Football.to_string_qb |> print_endline;
+  let _ =
+    print_endline
+      (Scrape.to_string_player (Scrape.Football.get_player_info name))
+  in
+  Scrape.Football.qback_scrape name
+  |> Scrape.Football.to_string_qb |> print_endline;
 
   let _ = print_endline "\n➾ Testing Runningback\n" in
   let name = "leonard fournette" in
-  let _ = print_endline (to_string_player (Football.get_player_info name)) in
-  Football.off_scrape name |> Football.to_string_off |> print_endline
+  let _ =
+    print_endline
+      (Scrape.to_string_player (Scrape.Football.get_player_info name))
+  in
+  Scrape.Football.off_scrape name
+  |> Scrape.Football.to_string_off |> print_endline
