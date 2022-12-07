7b25e72c3e8581c4fae5894f9ed50a3d77197e7f
Dustin Ngo <dpn29@cornell.edu>
2022-12-07 00:01:47 -0500

    updated command_line and parsing, created bin to implement the command line and user interaction

--- src/command_line.ml
+++ src/command_line.ml
@@ -1,19 +1,36 @@
-open ANSITerminal
+exception Empty
+exception Malformed
 
-let main () =
-  ANSITerminal.print_string [ ANSITerminal.red ]
-    "\n\nWelcome to the 3110 Sports Database.\n";
-  print_endline "Please enter the name of the league you would like to see.\n
-                 Supported leagues: NBA\n";
-  match read_line () with
-  | exception No_Input -> ()
-  | "NBA" -> nba
+type sport = | Basketball | Football | Baseball | Hockey | Soccer
 
-let nba =
-  print_endline "What player would you like to know about? Please give their
-                 full first and last name\n";
-  match read_line () with
-  | exception No_Input -> nba
-  | player -> bball_scrape player
+let sport_parse s =
+  match s with
+  | "" -> raise Empty
+  | "Basketball" -> Basketball
+  | "Football" -> Football
+  | "Baseball" -> Baseball
+  | "Hockey" -> Hockey
+  | "Soccer" -> Soccer
+  | _ -> raise Malformed
 
-let () = main ()
+let rec is_empty_helper n =
+  match n with
+  | [] -> true
+  | h :: t -> if h = "" then is_empty_helper t else false
+
+let is_empty n = 
+  match String.split_on_char ' ' n with
+  | [] -> true
+  | h :: t -> if h = "" then is_empty_helper t else false
+
+let parse_name n =
+  if is_empty n then raise Empty
+  else String.split_on_char ' ' n
+
+let parse_names n =
+  if is_empty n then raise Empty
+  else match String.split_on_char ',' n with
+  | [] -> raise Empty
+  | h :: [] -> raise Malformed
+  | h1 :: h2 :: [] -> List.map String.trim [h1; h2]
+  | _ -> raise Malformed
